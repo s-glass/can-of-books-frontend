9b17f4712be2a37bf910c81e3539f1005a3986c0
Dasha Burgos <dasha.lewis@gmail.com>
2023-03-27 23:11:07 -0400

    completed naviation routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
+// import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      books: [],
+      error: '',
+    }
+  };
+
   render() {
+    
     return (
       <>
         <Router>
@@ -19,6 +30,11 @@ class App extends React.Component {
             <Route 
               exact path="/"
               element={<BestBooks />}
+            >
+            </Route>
+            <Route 
+              exact path="/about"
+              element={<About />}
             >
             </Route>
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
