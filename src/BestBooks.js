9ddbc0d277538e73758918867a70d1dd9240a7bd
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-03-28 14:28:30 -0700

    add deleteBook functionality

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import { Component } from 'react';
 import axios from 'axios';
-import { Carousel, Container } from 'react-bootstrap';
+import { Carousel, Container, Button } from 'react-bootstrap';
 
-class BestBooks extends React.Component {
+class BestBooks extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -28,9 +28,40 @@ class BestBooks extends React.Component {
     }
   }
 
+  deleteBook = async (id) => {
+
+    try{
+      // Axios will send ID of cat to delete
+      
+      let url = `${process.env.REACT_APP_SERVER}/books/${id}`
+  
+      await axios.delete(url);
+  
+  
+      // Update state to then remove the delete books => use .filter() to get 
+      
+      let updatedBooks = this.state.books.filter(book => book._id !== id); // easy way to do this is {this.getBooks}
+  
+  
+      this.setState({
+        books: updatedBooks,
+      })
+  
+  
+  
+  
+    }catch(error){
+      console.log(error.response);
+    }
+  
+  }
+
   componentDidMount() {
     this.getBooks();
   }
+
+
+
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
@@ -49,6 +80,9 @@ class BestBooks extends React.Component {
 
               <Carousel>
                   {this.state.books.map((book, key) => (
+                  <div key={key}>
+                    <Book book={book}/> 
+
                     <Carousel.Item key={book._id}>
                       <img src="../img/Screenshot 2023-03-25 at 2.02.00 AM (2).png"
                         alt={book.description}
@@ -57,6 +91,7 @@ class BestBooks extends React.Component {
                         {book.title} is about {book.description}
                       </Carousel.Caption>
                     </Carousel.Item>
+                    </div>
                   ))}
               </Carousel>
             </Container>
@@ -67,5 +102,90 @@ class BestBooks extends React.Component {
     )
   }
 }
+class Book extends Component {
+  render() {
+    return (
+      <>
+      <p>{this.props.book.title} is about {this.props.book.description}</p>
+      <Button onClick= {() => {this.props.deleteBook(this.props.book._id)}}>Delete</Button>
+      </>
+    )
+  }
+}
 
 export default BestBooks;
+
+/* 
+//delete goes where state is which is here -- wherever state lives is where to update the delete state
+
+deleteBook = async (id) => {
+
+  try{
+    // Axios will send ID of cat to delete
+    
+    let url = `${process.env.REACT_APP_SERVER}/books/${id}`
+
+    await axios.delete(url);
+
+
+    // Update state to then remove the delete books => use .filter() to get 
+    
+    let updatedBooks = this.state.books.filter(book => book._id ! == id); // easy way to do this is {this.getBooks}
+
+
+    this.setState({
+      books: updatedBooks,
+    })
+
+
+
+
+  }catch{
+    console.log(error.response);
+  }
+
+}
+
+***** Create a MODAL that will pop up when a button is pressed to add a book **** // handler lives where state lives --- modal form can go else where (form needs fields for each part of the schema and the status)*****
+
+****handleBookSubmit -- builds the added book****
+
+** #1 comes from Form and builds Book Object **
+
+handleBookSubmit = (event) => {
+  event.preventDefault();
+
+  // Build Cat Object from Form Data
+  let bookObj = {
+    title: event.target.title.value  //--enter the values from forms --
+    description: event.target.description.value
+    status: event.target.status.checked   //--needs to be checked--
+  }
+  this.postBook(bookObj); // passed this handleSubmit function to the postBook function
+
+}
+
+
+
+***adds book to database***
+
+postBook = async (bookObj) => {
+
+  try{
+
+  let url = `${process.env.REACT_APP_SERVER}/cats`
+
+  let createdBook = await axios.post(url, bookObj) // --ON a post, 
+
+  this.setState({
+    books: [...this.state.books, createdBook.data],
+  })
+
+  } catch(error){
+    console.log(error.message)
+  }
+
+
+}
+
+*/
